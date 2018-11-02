19e03835d5e7e34c41ebf93b1ded1b071aaab150
Roberto <roberto@morales.com.co>
2018-11-01 20:49:35 -0500

    Imagen subida a storage

--- app/src/main/java/com/holamundo/personasmaterial/AgregarPersona.java
+++ app/src/main/java/com/holamundo/personasmaterial/AgregarPersona.java
@@ -2,6 +2,7 @@ package com.holamundo.personasmaterial;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,12 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -22,6 +29,8 @@ public class AgregarPersona extends AppCompatActivity {
     private String opc[];
     private ArrayList<Integer> fotos;
     private ImageView foto;
+    private Uri uri;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,7 @@ public class AgregarPersona extends AppCompatActivity {
         txtApellido = findViewById(R.id.txtApellido);
         txtNombre = findViewById(R.id.txtNombre);
         cmbsexo = findViewById(R.id.cmbSexo);
-        foto = findViewById(R.id.foto);
+        foto = findViewById(R.id.fotoe);
 
         fotos = new ArrayList<>();
         fotos.add(R.drawable.images);
@@ -43,6 +52,8 @@ public class AgregarPersona extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item,opc);
         cmbsexo.setAdapter(adapter);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
     }
 
     public int fotoAleatoria(){
@@ -65,6 +76,7 @@ public class AgregarPersona extends AppCompatActivity {
         sexo = cmbsexo.getSelectedItemPosition();
         Persona p = new Persona(id,foto,ced,nomb,appell,sexo);
         p.guardar();
+        subir_foto(foto);
         limpiar();
         Snackbar.make(v,getResources().getString(R.string.guardado_exitoso),Snackbar.LENGTH_SHORT)
                 .show();
@@ -99,6 +111,23 @@ public class AgregarPersona extends AppCompatActivity {
         i.setType("image/*");
         i.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(i,
-                "Seleccionar Foto"),1);
+                getResources().getString(R.string.seleccionarFoto)),1);
+    }
+    protected void onActivityResult(int requesCode,int resultCode,Intent data){
+        super.onActivityResult(requesCode,resultCode,data);
+
+        if(requesCode==1){
+            uri = data.getData();
+
+            if(uri != null){
+               foto.setImageURI(uri);
+
+            }
+        }
+    }
+
+    private void subir_foto(String foto){
+        StorageReference child = storageReference.child(foto);
+        UploadTask uploadTask = child.putFile(uri);
     }
 }
